d3eaceb1854987f573f27e1feb0f3ba82c1dab61
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-03-31 19:52:19 +0200

    adding similar movies feature

--- app/movies/[movie_id]/page.tsx
+++ app/movies/[movie_id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import Movie from '@/app/Movie';
 
 export const generateStaticParams = async () => {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
@@ -11,19 +12,22 @@ export const generateStaticParams = async () => {
 }
 
 const MovieDetail = async ({params}:any) => {
-    const {movie_id} = params;
+  const {movie_id} = params;
   const imagePath = "https://image.tmdb.org/t/p/original"
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${params.movie_id}?api_key=${process.env.API_KEY}`)
-    const data = await res.json()
-    console.log(data)
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.API_KEY}`)
+  const data = await res.json()
+  const similar = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${process.env.API_KEY}`)
+  const similarData = await similar.json();
+  const similarMovies = similarData.results.splice(0,8);
   return (
+    <>
     <div className='flex flex-col gap-8 items-center'>
         <Link className='bg-blue-600 p-5 text-lg rounded-lg' href="/">Home</Link>
-        <div className='text-center'>
+        <div className='text-center flex flex-col gap-2 items-center'>
             <h2 className='text-2xl'>{data.title}</h2>
             <h2 className='text-lg'>{data.release_date}</h2>
             <h2>Runtime: {data.runtime} minutes </h2>
-            <h2 className='bg-green-700 inline-block p-3 text-sm rounded-md'> {data.status} </h2>
+            <h2 className='bg-green-700 inline-block p-3 text-sm rounded-md w-fit'> {data.status} </h2>
             <Image
             className='my-8 m-auto'
             src={imagePath + data.poster_path}
@@ -35,6 +39,21 @@ const MovieDetail = async ({params}:any) => {
             <p>{data.overview}</p>
         </div>
     </div>
+    <div className='my-10'>
+      <h2 className='text-3xl'>Similar Movies</h2>
+      <div className='grid gap-16 lg:grid-cols-4 sm:grid-cols-2 p-6'>
+        {similarMovies.map((movie:any) => (
+          <Movie 
+          id={movie.id}
+          key={movie.id} 
+          title={movie.title}
+          release_date={movie.release_date} 
+          poster_path={movie.poster_path}
+          />
+        ))}
+      </div>
+    </div>
+    </>
   )
 }
 
